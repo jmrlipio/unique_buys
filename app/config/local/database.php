3cf2ba3b45589014c884649d0f8a505aa737c95e
Jone <jmrlipio@gmail.com>
2014-12-27 21:56:38 +0800

    created ubuntu branch

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'unique_buys',
 			'username'  => 'root',
-			'password'  => '1234aA',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
